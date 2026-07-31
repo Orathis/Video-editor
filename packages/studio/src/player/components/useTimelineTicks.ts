heygen-com/hyperframes

perf(studio): virtualize timeline clip windows

packages/studio/src/player/components/useTimelineTicks.ts
@@ -1,16 +1,21 @@
 import { useMemo } from "react";
 import { STUDIO_PREVIEW_FPS } from "../lib/time";
-import { generateTicks } from "./timelineLayout";
+import { generateTicks, getTimelineMajorTickInterval } from "./timelineLayout";
+import type { TimelineTimeRange } from "../lib/timelineClipIndex";
 
 export function useTimelineTicks(
   duration: number,
   pixelsPerSecond: number,
   timeDisplayMode: "time" | "frame",
-): { major: number[]; minor: number[] } {
+  renderTimeRange?: TimelineTimeRange,
+) {
   const frameRate = timeDisplayMode === "frame" ? STUDIO_PREVIEW_FPS : undefined;
-  const { major, minor } = useMemo(
-    () => generateTicks(duration, pixelsPerSecond, frameRate),
-    [duration, frameRate, pixelsPerSecond],
+  const ticks = useMemo(
+    () => generateTicks(duration, pixelsPerSecond, frameRate, renderTimeRange),
+    [duration, frameRate, pixelsPerSecond, renderTimeRange],
   );
-  return { major, minor };
+  return {
+    ...ticks,
+    majorTickInterval: getTimelineMajorTickInterval(duration, pixelsPerSecond, frameRate),
+  };
 }

Orathis/Video-editor

packages/studio/src/player/components/useTimelineTicks.ts
@@ -1,17 +1,22 @@
 import { useMemo } from "react";
 import { STUDIO_PREVIEW_FPS } from "../lib/time";
 import type { TimelineTimeDisplayMode } from "../../utils/studioUiPreferences";
-import { generateTicks } from "./timelineRulerGeometry";
+import type { TimelineTimeRange } from "../lib/timelineClipIndex";
+import { generateTicks, getTimelineMajorTickInterval } from "./timelineRulerGeometry";
 
 export function useTimelineTicks(
   duration: number,
   pixelsPerSecond: number,
   timeDisplayMode: TimelineTimeDisplayMode,
-): { major: number[]; minor: number[] } {
+  renderTimeRange?: TimelineTimeRange,
+) {
   const frameRate = timeDisplayMode === "frame" ? STUDIO_PREVIEW_FPS : undefined;
-  const { major, minor } = useMemo(
-    () => generateTicks(duration, pixelsPerSecond, frameRate),
-    [duration, frameRate, pixelsPerSecond],
+  const ticks = useMemo(
+    () => generateTicks(duration, pixelsPerSecond, frameRate, renderTimeRange),
+    [duration, frameRate, pixelsPerSecond, renderTimeRange],
   );
-  return { major, minor };
+  return {
+    ...ticks,
+    majorTickInterval: getTimelineMajorTickInterval(duration, pixelsPerSecond, frameRate),
+  };
 }